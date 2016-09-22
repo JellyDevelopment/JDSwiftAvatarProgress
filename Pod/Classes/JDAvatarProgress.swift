4218f65467591f5fc82325013e1b8b79696b1793
Keke Arif <kekearif@gmail.com>
2016-09-22 17:39:26 +0800

    Updated for Swift 3.0

--- Pod/Classes/JDAvatarProgress.swift
+++ Pod/Classes/JDAvatarProgress.swift
@@ -13,9 +13,9 @@ public let JDAvatarDefaultProgressBarLineWidth = 10.0
 public let JDAvatarDefaultBorderWidth = 5.0
 
 
-public class JDAvatarProgress: UIImageView, NSURLSessionTaskDelegate, NSURLSessionDownloadDelegate {
-
-    public typealias JDAvatarCompletionBlock = (image: UIImage?, error: NSError?) -> Void
+public class JDAvatarProgress: UIImageView, URLSessionTaskDelegate, URLSessionDownloadDelegate {
+    
+    public typealias JDAvatarCompletionBlock = (_ image: UIImage?, _ error: Error?) -> Void
     
     public var placeholderImage : UIImage!{
         
@@ -24,10 +24,9 @@ public class JDAvatarProgress: UIImageView, NSURLSessionTaskDelegate, NSURLSessi
             self.image = self.placeholderImage
         }
     }
-
-    public var urlSession : NSURLSession?
     
-    public var downloadTask : NSURLSessionDownloadTask!
+    public var urlSession : URLSession?
+    public var downloadTask : URLSessionDownloadTask!
     public var spinLayer : CAShapeLayer?
     public var progress : Double!
     public var tickness : Double!
@@ -80,16 +79,16 @@ public class JDAvatarProgress: UIImageView, NSURLSessionTaskDelegate, NSURLSessi
     override public func layoutSubviews() {
         
         super.layoutSubviews()
-
+        
         let bounds : CGRect = self.bounds
-        let outer : CGRect = CGRectInset(self.bounds, CGFloat(self.tickness/2.0), CGFloat(self.tickness/2.0))
+        let outer : CGRect = self.bounds.insetBy(dx: CGFloat(self.tickness/2.0), dy: CGFloat(self.tickness/2.0))
         
-        let outerPath: UIBezierPath = UIBezierPath(arcCenter: CGPointMake(CGRectGetMidX(outer), CGRectGetMidY(outer)), radius: self._radius(), startAngle: CGFloat(-M_PI_2), endAngle: CGFloat(2.0 * M_PI - M_PI_2), clockwise: true)
+        let outerPath: UIBezierPath = UIBezierPath(arcCenter: CGPoint(x: outer.midY, y: outer.midY), radius: self._radius(), startAngle: CGFloat(-M_PI_2), endAngle: CGFloat(2.0 * M_PI - M_PI_2), clockwise: true)
         
-        self.spinLayer?.path = outerPath.CGPath
+        self.spinLayer?.path = outerPath.cgPath
         self.spinLayer?.frame = bounds
         
-        UIGraphicsBeginImageContextWithOptions(bounds.size, false, UIScreen.mainScreen().scale)
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, UIScreen.main.scale)
         UIGraphicsEndImageContext()
         
         
@@ -106,48 +105,49 @@ public class JDAvatarProgress: UIImageView, NSURLSessionTaskDelegate, NSURLSessi
     }
     
     // MARK: Setters
+    
     public func setImageWithURLString(urlString : String){
         
         let url = NSURL(string: urlString)
         
-        self.setImageWithURL(url!, placeholder: nil, progressBarColor: nil, progressBarLineWidth: self.progressBarLineWidth, borderColor:nil, borderWidth: self.borderWidth, completion: nil)
+        self.setImageWithURL(urlImage: url!, placeholder: nil, progressBarColor: nil, progressBarLineWidth: self.progressBarLineWidth, borderColor:nil, borderWidth: self.borderWidth, completion: nil)
     }
     
     public func setImageWithURL(urlImage : NSURL){
         
-        self.setImageWithURL(urlImage, placeholder: nil, progressBarColor: nil, progressBarLineWidth: self.progressBarLineWidth, borderColor: nil, borderWidth: self.borderWidth, completion: nil)
+        self.setImageWithURL(urlImage: urlImage, placeholder: nil, progressBarColor: nil, progressBarLineWidth: self.progressBarLineWidth, borderColor: nil, borderWidth: self.borderWidth, completion: nil)
     }
     
     public func setImageWithURL(urlImage : NSURL, placeholder : UIImage){
-     
-        self.setImageWithURL(urlImage, placeholder: placeholder, progressBarColor: nil, progressBarLineWidth: self.progressBarLineWidth, borderColor: nil, borderWidth: self.borderWidth, completion: nil)
-
+        
+        self.setImageWithURL(urlImage: urlImage, placeholder: placeholder, progressBarColor: nil, progressBarLineWidth: self.progressBarLineWidth, borderColor: nil, borderWidth: self.borderWidth, completion: nil)
+        
     }
     
-    public func setImageWithURL(urlImage : NSURL, placeholder : UIImage, completion : JDAvatarCompletionBlock ){
+    public func setImageWithURL(urlImage : NSURL, placeholder : UIImage, completion : @escaping JDAvatarCompletionBlock ){
         
-        self.setImageWithURL(urlImage, placeholder: placeholder, progressBarColor: nil, progressBarLineWidth: self.progressBarLineWidth, borderColor: nil, borderWidth: self.borderWidth, completion: completion)
-
+        self.setImageWithURL(urlImage: urlImage, placeholder: placeholder, progressBarColor: nil, progressBarLineWidth: self.progressBarLineWidth, borderColor: nil, borderWidth: self.borderWidth, completion: completion)
+        
     }
-
+    
     public func setImageWithURL(urlImage : NSURL, placeholder : UIImage?, progressBarColor: UIColor?, progressBarLineWidth: Double, completion : JDAvatarCompletionBlock? ){
         
-        self.setImageWithURL(urlImage, placeholder: placeholder, progressBarColor: progressBarColor, progressBarLineWidth: progressBarLineWidth, borderColor: nil, borderWidth: self.borderWidth, completion: completion)
+        self.setImageWithURL(urlImage: urlImage, placeholder: placeholder, progressBarColor: progressBarColor, progressBarLineWidth: progressBarLineWidth, borderColor: nil, borderWidth: self.borderWidth, completion: completion)
     }
     
     public func setImageWithURL(urlImage : NSURL, placeholder : UIImage?, progressBarColor: UIColor?, progressBarLineWidth: Double, borderColor: UIColor?, borderWidth: Double, completion : JDAvatarCompletionBlock? ){
         
-        if (self.downloadTask != nil && self.downloadTask.state == NSURLSessionTaskState.Running ){
+        if (self.downloadTask != nil && self.downloadTask.state == URLSessionTask.State.running ){
             
             self._dismissProgressBar()
             
-            self.downloadTask.cancelByProducingResumeData({ (data) -> Void in
+            self.downloadTask.cancel(byProducingResumeData: { (data) -> Void in
                 
                 self.downloadTask = nil
                 
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-
-                    self.setImageWithURL(urlImage, placeholder: placeholder, progressBarColor: progressBarColor, progressBarLineWidth: progressBarLineWidth, borderColor: borderColor, borderWidth: borderWidth, completion: completion)
+                DispatchQueue.main.async(execute: { () -> Void in
+                    
+                    self.setImageWithURL(urlImage: urlImage, placeholder: placeholder, progressBarColor: progressBarColor, progressBarLineWidth: progressBarLineWidth, borderColor: borderColor, borderWidth: borderWidth, completion: completion)
                     
                 })
             })
@@ -173,72 +173,77 @@ public class JDAvatarProgress: UIImageView, NSURLSessionTaskDelegate, NSURLSessi
             
             self.progressBarLineWidth = progressBarLineWidth
             self.borderWidth = borderWidth
-
+            
             self.progress = 0.0
             self._initalizateProgressBar()
             
-            self.downloadTask = self.urlSession!.downloadTaskWithURL(urlImage)
+            self.downloadTask = self.urlSession!.downloadTask(with: urlImage as URL)
             self.downloadTask.resume()
         }
     }
     
-
+    
     // MARK: NSURLSession Delegate Methods
-    public func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+    
+    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        
         
         if (error != nil) {
             
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            DispatchQueue.main.async(execute: { () -> Void in
                 
-                if task.state != NSURLSessionTaskState.Completed {
+                if task.state != URLSessionTask.State.completed {
                     
                     self._dismissProgressBar()
                 }
                 
                 if (self.completionBlock != nil ) {
                     
-                    self.completionBlock(image: nil, error: error)
+                    self.completionBlock(nil, error)
                 }
             })
         }
-
+        
         
     }
-  public   
-    func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+    
+    
+    
+    public
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
         
         
-        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+        DispatchQueue.main.async() { () -> Void in
             
             let value : Double = Double(totalBytesWritten) / Double(totalBytesExpectedToWrite)
-           
-            self._setProgress(value, animated:true)
-        
+            
+            self._setProgress(progress: value, animated:true)
+            
         }
         
     }
     
-    public func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didFinishDownloadingToURL location: NSURL) {
-    
-        let data : NSData = NSData(contentsOfURL: location)!
-        let imageObtained : UIImage = UIImage(data: data)!
+    public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
         
-        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+        let data : NSData = NSData(contentsOf: location as URL)!
+        let imageObtained : UIImage = UIImage(data: data as Data)!
+        
+        DispatchQueue.main.async() { () -> Void in
             
-            self._setProgress(1.0, animated: false)
+            self._setProgress(progress: 1.0, animated: false)
             self._dismissProgressBar()
             
-            UIView.transitionWithView(self, duration: 0.3, options: UIViewAnimationOptions.TransitionCrossDissolve,
-                animations: { () -> Void in
-                    
-                    self.image = imageObtained
-                
+            UIView.transition(with: self, duration: 0.3, options: UIViewAnimationOptions.transitionCrossDissolve,
+                              animations: { () -> Void in
+                                
+                                self.image = imageObtained
+                                
                 },
-                completion: { (finished) -> Void in })
+                              completion: { (finished) -> Void in })
             
             if (self.completionBlock != nil) {
                 
-                self.completionBlock(image:imageObtained, error: nil)
+                self.completionBlock(imageObtained, nil)
             }
         }
     }
@@ -265,7 +270,7 @@ public class JDAvatarProgress: UIImageView, NSURLSessionTaskDelegate, NSURLSessi
             CATransaction.setDisableActions(true)
             
         }
-
+        
         //Añadido
         if self.spinLayer == nil{
             
@@ -274,7 +279,7 @@ public class JDAvatarProgress: UIImageView, NSURLSessionTaskDelegate, NSURLSessi
         
         self.spinLayer?.strokeEnd = CGFloat(self.progress)
         CATransaction.commit()
-    
+        
     }
     
     func _dismissProgressBar(){
@@ -284,28 +289,25 @@ public class JDAvatarProgress: UIImageView, NSURLSessionTaskDelegate, NSURLSessi
     }
     
     func _commonInit(){
-
-        //self.placeholderImage = UIImage(named: "empty_avatar")!
         
-        self.layer.cornerRadius = CGRectGetWidth(self.frame)/2;
+        self.layer.cornerRadius = self.frame.width/2;
         self.layer.masksToBounds = true
         
         //Default values
-        self.progressBarColor = UIColor.blueColor()
+        self.progressBarColor = UIColor.blue
         self.progressBarLineWidth = JDAvatarDefaultProgressBarLineWidth
         self.tickness = 1.0
         self.borderColor = UIColor(white: 0.9, alpha: 1.0)
         self.borderWidth = JDAvatarDefaultBorderWidth
-        
-        self.urlSession =  NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-            delegate: self,
-            delegateQueue: NSOperationQueue.mainQueue())
+        self.urlSession = URLSession(configuration: URLSessionConfiguration.default,
+                                     delegate: self,
+                                     delegateQueue: OperationQueue.main)
         
     }
     
     func _radius() -> CGFloat{
         
-        let r : CGRect = CGRectInset(self.bounds, CGFloat(self.tickness/2.0), CGFloat(self.tickness/2.0))
+        let r : CGRect = self.bounds.insetBy(dx: CGFloat(self.tickness/2.0), dy: CGFloat(self.tickness/2.0))
         let w = r.size.width
         let h = r.size.height
         
@@ -327,8 +329,8 @@ public class JDAvatarProgress: UIImageView, NSURLSessionTaskDelegate, NSURLSessi
         
         self.spinLayer = CAShapeLayer(layer: layer)
         self.spinLayer!.lineCap = "round"
-        self.spinLayer!.strokeColor = self.progressBarColor!.CGColor
-        self.spinLayer!.fillColor    = UIColor.clearColor().CGColor
+        self.spinLayer!.strokeColor = self.progressBarColor!.cgColor
+        self.spinLayer!.fillColor    = UIColor.clear.cgColor
         self.spinLayer!.lineWidth    = CGFloat(self.progressBarLineWidth!)
         self.spinLayer!.strokeEnd    = CGFloat(self.progress)
         
@@ -339,12 +341,12 @@ public class JDAvatarProgress: UIImageView, NSURLSessionTaskDelegate, NSURLSessi
         
         if (self.borderWidth != nil && self.borderColor != nil) {
             
-            self.layer.borderColor = self.borderColor!.CGColor
+            self.layer.borderColor = self.borderColor!.cgColor
             self.layer.borderWidth = CGFloat(self.borderWidth)
         }
         
         self.setNeedsDisplay()
     }
-
+    
     
 }
